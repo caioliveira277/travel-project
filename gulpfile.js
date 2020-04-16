1fe99c01e67f836148b15209fe3775f11469ea20
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-16 18:37:11 -0300

    initial settings

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const minify = require("gulp-babel-minify");
+const concat = require("gulp-concat");
+const order = require("gulp-order");
+const cleanCSS = require("gulp-clean-css");
+const pipeline = require("readable-stream").pipeline;
+const path = require("path");
+
+const resources = path.resolve(__dirname, "src", "resources");
+const assets = path.resolve(__dirname, "public", "assets");
+
+function MinfyCss() {
+  return pipeline(
+    gulp.src(`${resources}/css/*.css`),
+    cleanCSS(),
+    concat("styles.css"),
+    gulp.dest(`${assets}/css`)
+  );
+}
+
+function MinifyJs() {
+  return pipeline(
+    gulp.src(`${resources}/js/*.js`),
+    order(["globals.js", "functions.js", "events.js"]),
+    // babel({
+    //   presets: ["@babel/env"],
+    // })
+    // minify({
+    //   mangle: {
+    //     keepClassName: true,
+    //   },
+    // })
+    concat("app.js"),
+    gulp.dest(`${assets}/js`)
+  );
+}
+
+gulp.task("bundle", () => {
+  gulp.watch(`${resources}/**`, gulp.series(MinfyCss, MinifyJs));
+});
--- src/js/globals.js
+++
@@ -1,3 +0,0 @@
-const main = document.body.querySelector("main");
-const sliders = document.querySelectorAll(".slide");
-const header = document.querySelector("header");
