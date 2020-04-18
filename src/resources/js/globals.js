b359c4d89145581f180b02a57b4957d2fe9fa87b
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-18 12:48:42 -0300

    fix path global home

--- src/resources/js/globals.js
+++ src/resources/js/globals.js
@@ -3,6 +3,6 @@ const mouseTxt = mouse.querySelector("span");
 const burger = document.querySelector(".burger");
 let controller, slideScene, pageScene, detailScene, time;
 const url = {
-  home: "./index.html",
+  home: "#",
   fashion: "../../../index.html",
 };
